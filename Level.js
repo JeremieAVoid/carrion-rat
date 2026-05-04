c2f27649459d8bdd303dec2cd25e2c234ca043df
guromain <romain.guibert44@ynov.com>
2026-05-04 16:51:35 +0200

    [ MODIF ] mise en place clair de la classe

--- Level.js
+++ Level.js
@@ -1,27 +1,28 @@
-class Level{
+class Level {
     intensity;
     score = 0;
 
-    constructor(intensity){
+    constructor(intensity) {
         this.intensity = intensity;
     }
 
-    point(obstacle,rat){
-        if (obstacle.getPosY()<rat.getPosY()){
+    point(obstacle, rat) {
+        if (obstacle.getPosY() < rat.getPosY()) {
             this.score += 1;
         }
     }
 
-    game(largeur,hauteur){
-        let obstacles = [];
-        for (let i=0;i<this.intensity*5;i++){
-            const x = hauteur;
-            const y = obstacles.length*10+largeur;
-            const height = 100;
-            const width = 50+this.intensity;
-            const speed = 450+this.intensity*10;
-            obstacles.push(...obstacles,new Obstacle())
+    game(largeur, hauteur) {
+        const obstacles = [];
+        const groundY = hauteur - 20;
+        for (let i = 0; i < this.intensity * 5; i++) {
+            const width = 50 + this.intensity * 10;
+            const height = 80 + (i % 3) * 20;
+            const x = largeur + i * 400;
+            const y = groundY - height;
+            const speed = 300 + this.intensity * 30;
+            obstacles.push(new Obstacle(x, y, height, width, speed));
         }
-        return obstacles
+        return obstacles;
     }
 }
