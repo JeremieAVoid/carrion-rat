07fd395c65d4bf558db7295181a27f84812b1cf6
guromain <romain.guibert44@ynov.com>
2026-05-04 16:52:24 +0200

    [ MERGE ]

--- Rat.js
+++ Rat.js
@@ -3,54 +3,66 @@ class Rat {
     posY;
     height;
     width;
-    weight = 100;//g
-    gravity = 9.81; //m/s
+    weight = 100;
+    gravity = 9.81;
     velocityY = 0;
-    constructor(X,Y,height,width){
+
+    constructor(X, Y, height, width) {
         this.posX = X;
         this.posY = Y;
         this.height = height;
         this.width = width;
     }
 
-    getPosX(){
+    getPosX() {
         return this.posX;
     }
 
-    getPosY(){
+    getPosY() {
         return this.posY;
     }
 
-    setPosY(n){
+    setPosY(n) {
         this.posY = n;
     }
 
-    getHeight(){
+    getHeight() {
         return this.height;
     }
 
-    getWidth(){
+    getWidth() {
         return this.width;
     }
 
-    getVelocityY(){
+    getVelocityY() {
         return this.velocityY;
     }
 
-    getGravity(){
-        return this.weight*this.gravity;
+    getGravity() {
+        return this.weight * this.gravity;
     }
 
-    obstacle (obsta){
-        if (this.posX+this.length <= obsta.getLength()+obsta.getPosX || this.posX <= obsta.getLength()+obsta.getPosX){
-            if (this.posY+this.width >= obsta.getPosY){
-                this.posY = obsta.getPosY-this.width;
-            }
+    obstacle(obsta) {
+        const ratLeft = this.posX;
+        const ratRight = this.posX + this.width;
+        const ratTop = this.posY;
+        const ratBottom = this.posY + this.height;
+
+        const obsLeft = obsta.getPosX();
+        const obsRight = obsta.getPosX() + obsta.getWidth();
+        const obsTop = obsta.getPosY();
+        const obsBottom = obsta.getPosY() + obsta.getHeight();
+
+        const collides = ratRight > obsLeft && ratLeft < obsRight && ratBottom > obsTop && ratTop < obsBottom;
+        if (collides) {
+            this.setPosY(obsTop - this.height);
+            this.velocityY = 0;
         }
     }
 
-    jump(){
-        this.velocityY = -400;
+    jump() {
+        if (this.velocityY === 0) {
+            this.velocityY = -450;
+        }
     }
-
 }
