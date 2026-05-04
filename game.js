cc2c28c95fa53b498d10bb347589a8cba113a4f4
JeremieAVoid <jeremieadam44@proton.me>
2026-05-04 09:46:41 +0200

    [FIX] game.js

--- game.js
+++ game.js
@@ -4,93 +4,66 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
+//image du rat
 const ratImg = new Image();
 ratImg.src = "rat.png";
 
 ratImg.onload = () => {
-  console.log("Rat chargé !");
+    console.log("Rat image loaded successfully.");
 };
 
 //rat
-const player = {
-    x: 100,
-    y: canvas.height / 2,
-    width: 150,
-    height: 350,
-    velocityY: 0,
-    gravity: 800,
-    jumpForce: -350
-};
+const player = new Rat(100,canvas.height / 2,350,150)
 
+//obstacle
 
 const obstacle = {
-  x: canvas.width,
-  y: canvas.height - 150,
-  width: 60,
-  height: 150,
-  speed: 500
+    x: canvas.width,
+    y: canvas.height - 150,
+    width: 50,
+    height: 150,
+    speed: 300
 };
-
 
 window.addEventListener("keydown", (e) => {
     if (e.code === "Space") {
-        player.velocityY = player.jumpForce;
+        player.jump();
     }
 });
 
-window.addEventListener("mousedown", () => {
-  player.velocityY = player.jumpForce;
-});
-
-
 function update(deltaTime) {
-    player.velocityY += player.gravity * deltaTime;
+    player.velocityY += player.getGravity() * deltaTime;
     player.y += player.velocityY * deltaTime;
-        if (player.y + player.height > canvas.height) {
-            player.y = canvas.height - player.height;
-            player.velocityY = 0;
-        }
+    if (player.getPosY() + player.getHeight() > canvas.height) {
+        player.setPosY(canvas.height - player.getHeight());
+        player.velocityY = 0;
+    }
+    player.obstacle(obstacle);
     obstacle.x -= obstacle.speed * deltaTime;
-        if (obstacle.x + obstacle.width < 0) {
-            obstacle.x = canvas.width;
-        }
+    if (obstacle.x + obstacle.width < 0) {
+        obstacle.x = canvas.width;
+    }
 }
 
+//partie graphique
 function draw() {
-
-  // fond
-  ctx.fillStyle = "#a78484";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //fond
+    ctx.fillStyle = "#a78484";
+    ctx.fillRect(0, 0, canvas.height - 20, canvas.width, 20);
 
     //sol
-  ctx.fillRect(0, canvas.height - 20, canvas.width, 20);
+    ctx.fillRect(0, canvas.height - 20, canvas.width, 20);
 
     // obstacle (os / intestin)
     ctx.fillStyle = "#e8e2d0";
     ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
 
-  //rat
-  ctx.drawImage(
-    ratImg,
-    player.x,
-    player.y,
-    player.width,
-    player.height
-  );
+    //rat
+    ctx.drawImage(
+        ratImg,
+        player.x,
+        player.y,
+        player.width,
+        player.height
+    );
 }
-
-let lastTime = 0;
-
-function gameLoop(time) {
-
-  const deltaTime = (time - lastTime) / 1000;
-  lastTime = time;
-
-  update(deltaTime);
-  draw();
-
-  requestAnimationFrame(gameLoop);
-}
-
-requestAnimationFrame(gameLoop);
